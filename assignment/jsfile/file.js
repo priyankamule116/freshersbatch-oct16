3dcfce0db1934f2f1be2558d14d7c588864b3c78
unknown <priyankamule.116@gmail.com>
2022-06-09 20:12:22 +0530

    changes done

--- assignment/jsfile/file.js
+++ assignment/jsfile/file.js
@@ -9,6 +9,7 @@
 <button type="button" onclick="myFunction()">Click Me</button>
 
 <p>This example uses a full web URL to link to "myScript.js".</p>
+<p>This example uses a full web URL to link to "myScript.js".</p>
 <p>(myFunction is stored in "myScript.js")</p>
 
 <script src="https://www.w3schools.com/js/myScript.js"></script>
